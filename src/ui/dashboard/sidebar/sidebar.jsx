da866a23224a90e4c16a3cb4f8fe3419240176f1
Logan-JR <112017945+Logan-JR@users.noreply.github.com>
2024-04-11 22:02:58 -0400

    add navbar

--- src/ui/dashboard/sidebar/sidebar.jsx
+++ src/ui/dashboard/sidebar/sidebar.jsx
@@ -81,7 +81,13 @@ const Sidebar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.user}>
-        <Image className={styles.userImage} src='/noavatar.png' alt="" width='50' height='50' />
+        <Image
+          className={styles.userImage}
+          src="/noavatar.png"
+          alt=""
+          width="50"
+          height="50"
+        />
         <div className={styles.userDetail}>
           <span className={styles.username}>John Rivera</span>
           <span className={styles.userTitle}>Administrador</span>
@@ -97,6 +103,10 @@ const Sidebar = () => {
           </li>
         ))}
       </ul>
+      <button className={styles.logout}>
+        <MdLogout />
+        Logout
+      </button>
     </div>
   );
 };
