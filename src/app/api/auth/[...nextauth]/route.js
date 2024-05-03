1a991b25d1ff0f3ee4c9f885ee778cff0c0aee5a
Logan-JR <112017945+Logan-JR@users.noreply.github.com>
2024-05-02 20:32:19 -0400

    add next-auth

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,45 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectDB } from "@/utils/database";
+import User from "@/models/cpa/User";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "example@email.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "********",
+        },
+      },
+      async authorize(credentials, req) {
+        await connectDB();
+        console.log(credentials);
+        const userFound = await User.findOne({ email: credentials?.email });
+        if (!userFound) throw new Error("Invalid credentials");
+        const passwordMatch = credentials.password === userFound.password;
+        if (!passwordMatch) throw new Error("Invalid credentials");
+        return userFound;
+      },
+    }),
+  ],
+  callbacks: {
+    jwt({ account, token, user, profile, session }) {
+      if (user) token.user = user;
+      return token;
+    },
+    session({ session, token }) {
+      session.user = token.user;
+      return session;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,12 +1,32 @@
+"use client";
 import styles from "@/ui/login/login.module.css";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
+  const route = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const res = await signIn("credentials", {
+      email: formData.get("email"),
+      password: formData.get("password"),
+      redirect: false,
+    });
+    if (res?.ok) return route.push("/cpa");
+  };
+
   return (
     <div className={styles.container}>
-      <form action="" className={styles.form}>
+      <form onSubmit={handleSubmit} className={styles.form}>
         <p>Login</p>
-        <input type="text" placeholder="username" name="username" required />
-        <input type="password" placeholder="password" name="password" required />
+        <input type="email" placeholder="Email" name="email" required />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          required
+        />
         <button>Login</button>
       </form>
     </div>
